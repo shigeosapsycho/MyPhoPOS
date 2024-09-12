d8b09cd7c9c7d9c3efdd74b452865ecb1840cbf8
shigeosapsycho <157628053+shigeosapsycho@users.noreply.github.com>
2024-09-12 07:17:08 +0000

    first commit, prices.py needs to be finished the point of prices.py is so i can get it to print on the reciept

---
+++ POS.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from Prices import Prices
+
+# Only DoorDash, Grubhub, and Uber Eats are needed for the POS system
+class POS:
+    pass
---
+++ Prices.py
@@ -0,0 +1,57 @@
+class Menu:
+    def __init__(self):
+        self.Vermiceli = {
+            "#35": "",
+            "#35A": "",
+            "#36": "",
+            "#36A": "",
+            "#37": "",
+            "#38": "",
+            "#39": "",
+            "#40": "",
+            "#41": "",
+            "#42": "",
+            "#43": "",
+            "#43A": "",
+            "#43B": "",
+        }
+
+        self.Specials = {
+            "#44": "Chicken Fried Rice",
+            "#44A": "Shrimp Fried Rice",
+            "#45": "Combo Fried Rice",
+            "#46": "Seafood Fried Rice",
+            "#47": "Chicken Chow Mein",
+            "#49": "Seafood Chow Mein",
+            "49A": "Shrimp Chow Mein",
+            "#50": "Vegetarian Chow Mein",
+            "#51": "Combo Clay Pod Rice",
+            "#52": "Seafood Clay Pot Rice",
+        }
+
+        self.Drinks = [
+            "Thai Iced Tea",
+            "Smoothie",
+            "Coke",
+            "Sprite",
+            "Fanta (Orange)",
+            "Dr. Pepper",
+            "Diet Coke",
+            "Grass Jelly",
+            "Young Coconut",
+            "Jasmine Tea",
+            "Bottled Water",
+            "Plum Lemon Soda",
+            "Pickled Lemon Soda",
+            "Apple Juice",
+            "Vietnamese Iced Milk Coffee",
+            "Milk Tea",
+            "Milk Tea wtih Boba",
+            "Thai Iced Tea with Boba",
+            "Fresh Squeezed Lemonade",
+            "Fresh squeezed Soda Lemonade",
+        ]
+
+        self.PhoExtra = [
+            
+        ]
